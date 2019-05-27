17098b691ae47b1abbc0a410e4b006baf6bbd3b7
RaulMirandaOSF <raul.miranda@osf-commerce.com>
2019-05-27 13:02:48 -0300

    Building the Answer Routes

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var routes = require("./route");
+
+var jsonParser = require('body-parser').json;
+var logger = require('morgan');
+
+app.use(logger("dev"));
+app.use(jsonParser());
+app.use("/questions", routes);
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+    console.log("Express server is listening on port", port);
+});
